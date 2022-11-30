c6105ad965b661fdeb3cb268f447c9eab5af65a0
Tran-Hoang-Bao-Nam <118170038+Tran-Hoang-Bao-Nam@users.noreply.github.com>
2022-12-01 01:12:10 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = ' Võ Thùy Trang ';
+const giftUrl = '//bỏ trang wbb';
+const eventName = 'Merry Christmas';
+const titleCard = 'Gửi Em';
+const contentCard = 'Giáng Sinh tới rồi, a không có gì ngoài những lời chúc tốt đẹp nhất gửi tới em. Chúc em luôn xinh đẹp, vui vẻ, hạnh phúc, gặp nhiều may mắn trong công việc cũng như cuộc sống. Merry Christmas! Võ Thùy Trang';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '58bda205092cd072893d.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
